ab9d4d3b685d3341ac22b3160d7e8ff265b9c0b3
benjamin deering <ben_deering@jeepingben.net>
2016-02-20 13:24:43 -0500

    Minor UI improvements Very small improvements to the UI - fix fonts and charset. This is still ugly, but is good enough for trying out electron/cordova/etc.

--- wax-gui.js
+++ wax-gui.js
@@ -6,6 +6,7 @@ var tempMaxLabel = document.getElementById("tempMaxLabel");
 var conditionsForm = document.getElementById("conditionsForm");
 var getWaxButton = document.getElementById("getWax");
 var brandsDiv = document.getElementById("brandsDiv");
+var resultsText = document.getElementById("resultsText");
 var brandCheckboxes = [];
 
 tempSlider.oninput = function() {
@@ -71,7 +72,12 @@ conditionsForm.onsubmit = function() {
 
 function showWax(wax) {
     resultsDiv.className = "visibleResult";
-    resultsDiv.innerHTML += " " + wax.color;
+    
+    if (wax.brand === 'none') {
+    	resultsText.textContent = "No suitable wax found for these conditions";
+    } else {
+    	resultsText.textContent =  wax.brand + " " + wax.name;
+    }
     if (!!wax.picture) {
         var waxImage = document.getElementById("waxImage");
         waxImage.src = 'images/' + wax.picture;
